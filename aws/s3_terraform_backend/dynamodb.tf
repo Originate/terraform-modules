f96ec736d17b28225fc5afab98115bfe1b2cb094
Christopher Kim <christopher.kim@originate.com>
2021-04-27 11:11:45 -0700

    Rename Terraform lock table to avoid conflicts

--- aws/s3_terraform_backend/dynamodb.tf
+++ aws/s3_terraform_backend/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "TerraformLock"
+  name         = "${var.stack}-terraform-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
