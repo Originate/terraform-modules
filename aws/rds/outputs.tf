f25dd2fee1824f2dde151d19c93d2849aae76bc1
Christopher Kim <christopher.kim@originate.com>
2021-07-09 15:06:26 -0700

    Fixes for RDS module breaking changes

--- aws/rds/outputs.tf
+++ aws/rds/outputs.tf
@@ -1,17 +1,17 @@
 output "host" {
-  value = module.rds.this_db_instance_address
+  value = module.rds.db_instance_address
 }
 
 output "port" {
-  value = module.rds.this_db_instance_port
+  value = module.rds.db_instance_port
 }
 
 output "database" {
-  value = module.rds.this_db_instance_name
+  value = module.rds.db_instance_name
 }
 
 output "username" {
-  value     = module.rds.this_db_instance_username
+  value     = module.rds.db_instance_username
   sensitive = true
 }
 
