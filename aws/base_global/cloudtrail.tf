387af8406b287fbb8affd7aed1776d406c3a1c45
Christopher Kim <christopher.kim@originate.com>
2022-07-21 10:36:40 -0700

    fix: Replace cloudtrail S3 bucket deprecated parameters with new resources

--- aws/base_global/cloudtrail.tf
+++ aws/base_global/cloudtrail.tf
@@ -1,12 +1,18 @@
 resource "aws_s3_bucket" "cloudtrail" {
   bucket = "${var.stack}-cloudtrail-logs"
-  acl    = "private"
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_acl" "cloudtrail" {
+  bucket = aws_s3_bucket.cloudtrail.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "cloudtrail" {
+  bucket = aws_s3_bucket.cloudtrail.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
